98b7c19c2dae88a1093aab34032d1163da23de1c
Zharkov-Nikita <nikita.zharkov.03@mail.ru>
2024-09-15 19:43:14 +0700

    Создание пользовательского интерфейса 4

--- ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.ItemsTabPage = new System.Windows.Forms.TabPage();
+            this.itemsTab1 = new ObjectOrientedPractics.View.Tabs.ItemsTab();
+            this.tabControl1.SuspendLayout();
+            this.ItemsTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.ItemsTabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(606, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // ItemsTabPage
+            // 
+            this.ItemsTabPage.Controls.Add(this.itemsTab1);
+            this.ItemsTabPage.Location = new System.Drawing.Point(4, 22);
+            this.ItemsTabPage.Name = "ItemsTabPage";
+            this.ItemsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ItemsTabPage.Size = new System.Drawing.Size(598, 424);
+            this.ItemsTabPage.TabIndex = 0;
+            this.ItemsTabPage.Text = "Items";
+            this.ItemsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // itemsTab1
+            // 
+            this.itemsTab1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.itemsTab1.Location = new System.Drawing.Point(3, 3);
+            this.itemsTab1.Name = "itemsTab1";
+            this.itemsTab1.Size = new System.Drawing.Size(592, 418);
+            this.itemsTab1.TabIndex = 0;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(606, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.tabControl1.ResumeLayout(false);
+            this.ItemsTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage ItemsTabPage;
+        private View.Tabs.ItemsTab itemsTab1;
     }
 }
 
---
+++ ObjectOrientedPractics/View/Tabs/ItemsTab.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class ItemsTab
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ItemIdTextBox = new System.Windows.Forms.TextBox();
+            this.ItemCostTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ItemNameTextBox = new System.Windows.Forms.TextBox();
+            this.ItemDescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ItemAddButton = new System.Windows.Forms.Button();
+            this.ItemRemoveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ItemsListBox
+            // 
+            this.ItemsListBox.FormattingEnabled = true;
+            this.ItemsListBox.Location = new System.Drawing.Point(14, 30);
+            this.ItemsListBox.Name = "ItemsListBox";
+            this.ItemsListBox.Size = new System.Drawing.Size(200, 342);
+            this.ItemsListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Items";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(220, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selected Item";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Cost:";
+            // 
+            // ItemIdTextBox
+            // 
+            this.ItemIdTextBox.Location = new System.Drawing.Point(258, 37);
+            this.ItemIdTextBox.Name = "ItemIdTextBox";
+            this.ItemIdTextBox.Size = new System.Drawing.Size(111, 20);
+            this.ItemIdTextBox.TabIndex = 5;
+            // 
+            // ItemCostTextBox
+            // 
+            this.ItemCostTextBox.Location = new System.Drawing.Point(258, 62);
+            this.ItemCostTextBox.Name = "ItemCostTextBox";
+            this.ItemCostTextBox.Size = new System.Drawing.Size(111, 20);
+            this.ItemCostTextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(220, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Name:";
+            // 
+            // ItemNameTextBox
+            // 
+            this.ItemNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ItemNameTextBox.Location = new System.Drawing.Point(223, 107);
+            this.ItemNameTextBox.Multiline = true;
+            this.ItemNameTextBox.Name = "ItemNameTextBox";
+            this.ItemNameTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ItemNameTextBox.Size = new System.Drawing.Size(359, 77);
+            this.ItemNameTextBox.TabIndex = 8;
+            // 
+            // ItemDescriptionTextBox
+            // 
+            this.ItemDescriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ItemDescriptionTextBox.Location = new System.Drawing.Point(223, 210);
+            this.ItemDescriptionTextBox.Multiline = true;
+            this.ItemDescriptionTextBox.Name = "ItemDescriptionTextBox";
+            this.ItemDescriptionTextBox.Size = new System.Drawing.Size(359, 114);
+            this.ItemDescriptionTextBox.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Description:";
+            // 
+            // ItemAddButton
+            // 
+            this.ItemAddButton.Location = new System.Drawing.Point(14, 378);
+            this.ItemAddButton.Name = "ItemAddButton";
+            this.ItemAddButton.Size = new System.Drawing.Size(75, 32);
+            this.ItemAddButton.TabIndex = 11;
+            this.ItemAddButton.Text = "Add";
+            this.ItemAddButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemRemoveButton
+            // 
+            this.ItemRemoveButton.Location = new System.Drawing.Point(95, 378);
+            this.ItemRemoveButton.Name = "ItemRemoveButton";
+            this.ItemRemoveButton.Size = new System.Drawing.Size(75, 32);
+            this.ItemRemoveButton.TabIndex = 12;
+            this.ItemRemoveButton.Text = "Remove";
+            this.ItemRemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemsTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ItemRemoveButton);
+            this.Controls.Add(this.ItemAddButton);
+            this.Controls.Add(this.ItemDescriptionTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ItemNameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ItemCostTextBox);
+            this.Controls.Add(this.ItemIdTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ItemsListBox);
+            this.Name = "ItemsTab";
+            this.Size = new System.Drawing.Size(595, 418);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ItemsListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ItemIdTextBox;
+        private System.Windows.Forms.TextBox ItemCostTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ItemNameTextBox;
+        private System.Windows.Forms.TextBox ItemDescriptionTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ItemAddButton;
+        private System.Windows.Forms.Button ItemRemoveButton;
+    }
+}
